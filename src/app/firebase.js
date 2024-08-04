9965248ab9d32cbab4ac2dcf2d6d975af13afa7c
Zaryab Khan <zaryabcom123@gmail.com>
2024-08-05 00:18:52 +0500

    error solved: ana;ytic build error

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,7 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,8 +18,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
-const auth = getAuth(app);
 
-export { firestore, auth };
+export { firestore };
